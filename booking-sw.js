cb46fa9a5e2cb1b372c2d278c5cbdeae5e72cf7d
kantasantosh <kantasantosh@gmail.com>
2026-06-15 20:05:41 +0530

    Add files via upload

--- booking-sw.js
+++ booking-sw.js
@@ -1,4 +1,4 @@
-const CACHE='booking-v12';
+const CACHE='booking-v13';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{
   e.waitUntil(
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pilates-v24';
+const CACHE='pilates-v25';
 const ASSETS=['./index.html', './icon192.png', './icon512.png', './manifest.json'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
