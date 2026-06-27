3b2e6f10b17af2168a298e827fb0845ee18b8687
kantasantosh <kantasantosh@gmail.com>
2026-06-28 03:26:40 +0530

    Add files via upload

--- booking-sw.js
+++ booking-sw.js
@@ -1,12 +1,15 @@
-const CACHE='booking-v16';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
+const CACHE='booking-v17';
+const ASSETS=['./booking.html'];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
 });
-self.addEventListener('fetch',e=>{
-  // network-first; fall back to cache only if offline
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>caches.match(e.request))
+  );
 });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pilates-v99b';
+const CACHE='pilates-v99d';
 const ASSETS=['./','./index.html','./icon192.png','./icon512.png','./manifest.json'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
