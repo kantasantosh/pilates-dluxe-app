089e31d64e9368fc8933811c43dbb0b2745abaff
kantasantosh <kantasantosh@gmail.com>
2026-06-28 04:18:52 +0530

    Add files via upload

--- booking-sw.js
+++ booking-sw.js
@@ -8,6 +8,9 @@ self.addEventListener('activate', e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
+self.addEventListener('message', e=>{
+  if(e.data==='skipWaiting')self.skipWaiting();
+});
 self.addEventListener('fetch', e=>{
   e.respondWith(
     fetch(e.request).catch(()=>caches.match(e.request))
