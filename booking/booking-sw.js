c7712de53071c69ab15c3ddb17f2a2fb99e7cfdc
kantasantosh <kantasantosh@gmail.com>
2026-07-29 14:55:07 +0530

    Add files via upload

--- booking/booking-sw.js
+++ booking/booking-sw.js
@@ -1,4 +1,4 @@
-const CACHE='booking-v59';
+const CACHE='booking-v60';
 const ASSETS=['./','./index.html'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
