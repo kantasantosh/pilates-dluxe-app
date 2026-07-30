4d65dc1ac2958f6502444b7d350aed264203fc4b
kantasantosh <kantasantosh@gmail.com>
2026-07-30 23:48:13 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pilates-v147.4';
+const CACHE='pilates-v147.6';
 const ASSETS=['./','./index.html','./icon192.png','./icon512.png','./manifest.json'];
 self.addEventListener('install', e=>{
   self.skipWaiting();
